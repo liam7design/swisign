edecb4d16c3b23aef275ebb57708c41f23fd7aa4
liam7design <lims7design@gmail.com>
2025-05-20 10:10:41 +0900

    이벤트 버블링 수정 (구조 변경)

--- src/components/localsale/LocalSaleItem.js
+++ src/components/localsale/LocalSaleItem.js
@@ -35,44 +35,44 @@ const LocalSaleItem = ({
 }) => {
   return (
     <LocalSaleItemCard variant="outlined">
-      <CardActionArea 
-        disableRipple 
-        onClick={() => onItemClick(item.id)}
-        sx={{ height: '100%'}}
-        >
-        <Box sx={{ position: 'relative' }}>
+      <Box sx={{ position: 'relative' }}>
+        <CardActionArea 
+          disableRipple 
+          onClick={() => onItemClick(item.id)}
+          sx={{ height: '100%'}}
+          >
           <CardMedia
             component="img"
             image={item.image}
             alt={item.propertyType}
             sx={{ width: '100%', aspectRatio: '4 / 3', objectFit: 'cover', cursor: 'pointer' }}
           />
-          <IconButton
-            sx={{ position: 'absolute', top: 8, right: 8, padding: 0, color: 'white', backgroundColor: 'transparent' }}
-            onClick={(e) => {
-              e.stopPropagation();
-              onLikeToggle(item.id);
-            }}
-            disableRipple
-          >
-            {liked ? <LikedIconOn stroke="white" /> : <LikedIconOff stroke="white" />}
-          </IconButton>
-        </Box>
-        <CardContent sx={{ height: '100%', pt: 1.5, pb: 2 }}>
-          <ItemTit variant="h6" noWrap>{item.saleType} {item.price}</ItemTit>
-          <ItemCont variant="body2" noWrap>{item.size} - {item.floor}</ItemCont>
-          <ItemCont variant="body2" noWrap>{item.address}</ItemCont>
-          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.4, mt: 1.5 }}>
-            {item.conditions.map((condition, index) => (
-              <Chip 
-                key={index} 
-                label={condition} 
-                size="small"
-              />
-            ))}
-          </Box>
-        </CardContent>
-      </CardActionArea>
+          <CardContent sx={{ height: '100%', pt: 1.5, pb: 2 }}>
+            <ItemTit variant="h6" noWrap>{item.saleType} {item.price}</ItemTit>
+            <ItemCont variant="body2" noWrap>{item.size} - {item.floor}</ItemCont>
+            <ItemCont variant="body2" noWrap>{item.address}</ItemCont>
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.4, mt: 1.5 }}>
+              {item.conditions.map((condition, index) => (
+                <Chip 
+                  key={index} 
+                  label={condition} 
+                  size="small"
+                />
+              ))}
+            </Box>
+          </CardContent>
+        </CardActionArea>
+        <IconButton
+          sx={{ position: 'absolute', top: 8, right: 8, padding: 0, color: 'white', backgroundColor: 'transparent' }}
+          onClick={(e) => {
+            e.stopPropagation();
+            onLikeToggle(item.id);
+          }}
+          disableRipple
+        >
+          {liked ? <LikedIconOn stroke="white" /> : <LikedIconOff stroke="white" />}
+        </IconButton>
+      </Box>
     </LocalSaleItemCard>
   );
 };
