95fb46b8e0292a76130e4b9e08c3208db1a6baff
liam7design <lims7design@gmail.com>
2025-05-20 10:13:49 +0900

    리엑트 문법 규칙 오류 수정 (카멜케이스 적용)

--- src/assets/icons/SvgIcons.js
+++ src/assets/icons/SvgIcons.js
@@ -1,50 +1,50 @@
 
 export const LikedIconOff = (props, stroke) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
-    <path d="M22 9C22 5.69 19.65 3 16.75 3C14.65 3 12.84 4.42 12 6.46C11.16 4.42 9.35 3 7.25 3C4.35 3 2 5.69 2 9C2 10.45 2.5 11.73 3.11 12.85C4.34 15.22 6.8 18.11 11.48 20.86C11.79 21.04 12.2 21.04 12.51 20.86C17.18 18.11 19.71 15.28 20.94 12.92C21.55 11.75 22 10.46 22 9Z" stroke={stroke} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+    <path d="M22 9C22 5.69 19.65 3 16.75 3C14.65 3 12.84 4.42 12 6.46C11.16 4.42 9.35 3 7.25 3C4.35 3 2 5.69 2 9C2 10.45 2.5 11.73 3.11 12.85C4.34 15.22 6.8 18.11 11.48 20.86C11.79 21.04 12.2 21.04 12.51 20.86C17.18 18.11 19.71 15.28 20.94 12.92C21.55 11.75 22 10.46 22 9Z" stroke={stroke} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
   </svg>
 );
 
 export const LikedIconOn = (props, stroke) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
-    <path d="M22 9C22 5.69 19.65 3 16.75 3C14.65 3 12.84 4.42 12 6.46C11.16 4.42 9.35 3 7.25 3C4.35 3 2 5.69 2 9C2 10.45 2.5 11.73 3.11 12.85C4.34 15.22 6.8 18.11 11.48 20.86C11.79 21.04 12.2 21.04 12.51 20.86C17.18 18.11 19.71 15.28 20.94 12.92C21.55 11.75 22 10.46 22 9Z" fill="#F63C4A" stroke={stroke} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+    <path d="M22 9C22 5.69 19.65 3 16.75 3C14.65 3 12.84 4.42 12 6.46C11.16 4.42 9.35 3 7.25 3C4.35 3 2 5.69 2 9C2 10.45 2.5 11.73 3.11 12.85C4.34 15.22 6.8 18.11 11.48 20.86C11.79 21.04 12.2 21.04 12.51 20.86C17.18 18.11 19.71 15.28 20.94 12.92C21.55 11.75 22 10.46 22 9Z" fill="#F63C4A" stroke={stroke} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
   </svg>
 );
 
 export const SizeIcon = (props, stroke) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
-    <rect vector-effect="non-scaling-stroke" x="2.7" y="2.7" width="18.6" height="18.6" rx=".1" stroke={stroke} stroke-width="1.4"></rect>
-    <path vector-effect="non-scaling-stroke" d="M6.7 11V6.7H11m6.3 6.3v4.3H13" stroke={stroke} stroke-width="1.4" stroke-linejoin="round"></path>
+    <rect vectorEffect="non-scaling-stroke" x="2.7" y="2.7" width="18.6" height="18.6" rx=".1" stroke={stroke} strokeWidth="1.4"></rect>
+    <path vectorEffect="non-scaling-stroke" d="M6.7 11V6.7H11m6.3 6.3v4.3H13" stroke={stroke} strokeWidth="1.4" strokeLinejoin="round"></path>
   </svg>
 );
 
 export const RoomIcon = (props, stroke) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
-    <rect vector-effect="non-scaling-stroke" x="2.7" y="2.7" width="18.6" height="18.6" rx=".1" stroke={stroke} stroke-width="1.4"></rect>
-    <path vector-effect="non-scaling-stroke" d="M2.5 16H15" stroke={stroke} stroke-width="1.4"></path>
-    <path vector-effect="non-scaling-stroke" d="M15 2.7v18.6" stroke={stroke} stroke-width="1.4" stroke-linejoin="round"></path>
+    <rect vectorEffect="non-scaling-stroke" x="2.7" y="2.7" width="18.6" height="18.6" rx=".1" stroke={stroke} strokeWidth="1.4"></rect>
+    <path vectorEffect="non-scaling-stroke" d="M2.5 16H15" stroke={stroke} strokeWidth="1.4"></path>
+    <path vectorEffect="non-scaling-stroke" d="M15 2.7v18.6" stroke={stroke} strokeWidth="1.4" strokeLinejoin="round"></path>
   </svg>
 );
 
 export const ParkingIcon = (props, stroke) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
-    <circle vector-effect="non-scaling-stroke" cx="12" cy="12" r="9.3" stroke={stroke} stroke-width="1.4" />
-    <path vector-effect="non-scaling-stroke" d="M9.5 16V8H13a2.5 2.5 0 0 1 0 5H9.5" stroke={stroke} stroke-width="1.4" stroke-linecap="square" />
+    <circle vectorEffect="non-scaling-stroke" cx="12" cy="12" r="9.3" stroke={stroke} strokeWidth="1.4" />
+    <path vectorEffect="non-scaling-stroke" d="M9.5 16V8H13a2.5 2.5 0 0 1 0 5H9.5" stroke={stroke} strokeWidth="1.4" strokeLinecap="square" />
   </svg>
 );
 
 export const FloorIcon = (props, stroke) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
-    <path vector-effect="non-scaling-stroke" d="M19.3 21.5V2.8a.1.1 0 0 0-.1-.1H4.8a.1.1 0 0 0-.1.1v18.7" stroke={stroke} stroke-width="1.4" stroke-linejoin="round"></path>
-    <path vector-effect="non-scaling-stroke" d="M2 21.3h20M8 7h3m2 0h3m-8 4h3m2 0h3m-8 4h3m2 0h3" stroke={stroke} stroke-width="1.4"></path>
+    <path vectorEffect="non-scaling-stroke" d="M19.3 21.5V2.8a.1.1 0 0 0-.1-.1H4.8a.1.1 0 0 0-.1.1v18.7" stroke={stroke} strokeWidth="1.4" strokeLinejoin="round"></path>
+    <path vectorEffect="non-scaling-stroke" d="M2 21.3h20M8 7h3m2 0h3m-8 4h3m2 0h3m-8 4h3m2 0h3" stroke={stroke} strokeWidth="1.4"></path>
   </svg>
 );
 
 export const DateIcon = (props, stroke) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
-    <path vector-effect="non-scaling-stroke" d="M17 2v4.4M7 2v4.4" stroke={stroke} stroke-width="1.4" stroke-linejoin="bevel"></path>
-    <path vector-effect="non-scaling-stroke" d="M2.7 4.4a.1.1 0 0 1 .1-.1h18.4a.1.1 0 0 1 .1.1v15.8a.1.1 0 0 1-.1.1H2.8a.1.1 0 0 1-.1-.1V4.4Z" stroke={stroke} stroke-width="1.4"></path>
-    <path vector-effect="non-scaling-stroke" d="M2.5 9h19" stroke={stroke} stroke-width="1.4" stroke-linejoin="bevel"></path>
+    <path vectorEffect="non-scaling-stroke" d="M17 2v4.4M7 2v4.4" stroke={stroke} strokeWidth="1.4" strokeLinejoin="bevel"></path>
+    <path vectorEffect="non-scaling-stroke" d="M2.7 4.4a.1.1 0 0 1 .1-.1h18.4a.1.1 0 0 1 .1.1v15.8a.1.1 0 0 1-.1.1H2.8a.1.1 0 0 1-.1-.1V4.4Z" stroke={stroke} strokeWidth="1.4"></path>
+    <path vectorEffect="non-scaling-stroke" d="M2.5 9h19" stroke={stroke} strokeWidth="1.4" strokeLinejoin="bevel"></path>
   </svg>
 );
 
