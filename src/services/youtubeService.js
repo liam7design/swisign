28fe809022cecbd8a1b3bfdf7e9a61ce50efe98e
Liam Lim <lims7pub@gmail.com>
2025-06-11 11:33:59 +0900

    유튜브 API 수정 (코드 정리)

--- src/services/youtubeService.js
+++ src/services/youtubeService.js
@@ -1,19 +1,17 @@
 import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 const PLAYLIST_ID = 'PLVI3CAcQB7GM7pBqn8WYVkSKn2QfUbS2E';
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+const API_URL = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${PLAYLIST_ID}&maxResults=50&key=${API_KEY}`;
 
 export const fetchYoutubeVideos = async () => {
   try {
-    const response = await axios.get(
-      `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${PLAYLIST_ID}&maxResults=50&key=${API_KEY}`
-    );
-
+    const response = await axios.get(API_URL);
     return response.data.items.map((item, index) => ({
       id: index + 1,
       title: item.snippet.title,
       date: item.snippet.publishedAt.split('T')[0],
-      views: 0, // 조회수는 별도의 API 호출이 필요합니다
+      views: 0,
       videoId: item.snippet.resourceId.videoId,
       content: item.snippet.title
     }));
