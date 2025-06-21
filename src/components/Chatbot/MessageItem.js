6d798cab17b1147b49acec25f1137600f4590e0d
Liam Lim <lims7design@gmail.com>
2025-06-21 11:56:54 +0900

    불필요한 useEffect 제거, 로깅 로직 통합

--- src/components/Chatbot/MessageItem.js
+++ src/components/Chatbot/MessageItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Box, Typography, Button, FormControlLabel, Checkbox } from '@mui/material';
 import QuickReplyButton from './QuickReplyButton';
 
